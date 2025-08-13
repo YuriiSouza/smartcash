68c51442bca00f31a61a83292960b18c98379cb8
Yuri Souza <yurisouza@mac.local>
2025-08-13 17:23:35 -0300

    fix: configurate the authOptions in lib

--- my-app/lib/auth.ts
+++ my-app/lib/auth.ts
@@ -9,30 +9,10 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
       // opcional: allowDangerousEmailAccountLinking: true
     }),
     // GitHubProvider({ clientId: process.env.GITHUB_ID!, clientSecret: process.env.GITHUB_SECRET! }),
-  ],
-  session: { strategy: "jwt" }, // com Mongo, JWT costuma ser mais simples
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        // coloca o id do User do Prisma no token
-        token.userId = (user as any).id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session?.user && token?.userId) {
-        (session.user as any).id = token.userId as string;
-      }
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/login", // sua página de login
-  },
-  // debug: process.env.NODE_ENV === "development",
+  ]
 };
